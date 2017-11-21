4415b14064d2501f4208c66b87fa847f27db1a06
Peter Kong <bansuki@gmail.com>
2017-11-21 10:38:54 -0600

    added firebase to about

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -8,7 +8,7 @@ class Hero extends Component {
         <h2>Web and Doctools Developer</h2>
         <p>
         Welcome! I am a full stack developer and documentation tools expert based in Austin, Texas.
-        I work mainly in <b>React</b>, <b>Node</b> and <b>Rails</b>.
+        I work mainly in <b>React</b>, <b>Node</b>, <b>Firebase</b> and <b>Rails</b>.
         </p>
         <p>
         Here you can learn <b><a href="#about">about me</a></b>,
