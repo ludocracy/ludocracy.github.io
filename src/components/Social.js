70bfa056418e9114b5d339911253273cd142e9cc
Peter Kong <bansuki@gmail.com>
2017-11-24 22:06:17 -0600

    social done and styling

--- src/components/Social.js
+++ src/components/Social.js
@@ -5,8 +5,11 @@ class Social extends Component {
   render() {
     return (
       <div className="Social">
-        <p> social media links </p>
-        <p><a>download my resumé</a>.</p>
+        <h2>My web presence</h2>
+        <p>My <b><a rel="noopener noreferrer" target="_blank" href="https://www.linkedin.com/in/pekong/">LinkedIn</a></b> profile has more details on my professional history, and I respond to messages promptly.</p>
+        <p>I also write about software, social issues and climate change on <b><a rel="noopener noreferrer" target="_blank" href="https://medium.com/@peterkong">Medium</a></b>.</p>
+        <p>And you can browse the source code for my many projects on <b><a rel="noopener noreferrer" target="_blank" href="https://github.com/ludocracy">GitHub</a></b>.</p>
+        <p>If you are interested in hiring me, please <b><a rel="noopener noreferrer" target="_blank" href="https://ludocracy.github.io/random-static-files/outcomes/peter-kong-resume.docx">download my résumé</a></b>.</p>
       </div>
     );
   }
